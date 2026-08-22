8f8111ea28960f76434779c6d835d8dd59e496d5
pubg-store <alphaxgaming4ever@gmail.com>
2026-08-22 22:39:28 +0800

    Refactor server.py for cleaner code structure

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify
 import requests
 import os
 
@@ -55,10 +55,3 @@ def order():
         print(error)
 
         return jsonify({"success": False}), 500
-
-
-if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=5000
-    )
